03e10ed8b7d8d081a38fdec817a8140f9197f073
ADooNeX <168536495+ADooNeX@users.noreply.github.com>
2024-04-30 13:24:35 +0300

    Add files via upload

---
+++ lab2/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 2;
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        int c = 55;
+        System.out.println(c/10+c%10);
+
+        double d = 8.5;
+        System.out.println((int)(d+0.5));
+
+    }
+}
